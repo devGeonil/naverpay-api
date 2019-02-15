664a2990b2bc3122944e22ebac0b03ee9152da4f
devGeonil <jgi92@naver.com>
2019-02-15 17:27:29 +0900

    need view engine

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import path from 'path';
+
 import morgan from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
+
 const app = express();
+
+//app.use(express.static(__dirname + '/'));
+app.set('view engine', 'pug');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,9 +22,7 @@ app.use(morgan("dev"));
 
 const homeController = (req, res) => {
   const request_params = req.query;
-
-
-  res.send("home");
+  res.render("index", {name:"geonil"});
 }
 
 app.get('/', homeController);
